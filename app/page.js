e93a2d43e9ddd2cdf1d822b6b2f9bdcc2fbf84da
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-11-01 06:52:51 +0200

    wip
    add more pages to layout

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
 import Main from "@/components/Main";
+import Contributors from "@/components/Contributors";
+import About from "@/components/About";
+
 export default function Home() {
   return (
     <div className="">
       <Main />
+      <About />
+      <Contributors />
     </div>
   );
 }
